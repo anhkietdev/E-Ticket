ce78c3beeb6a145b7f6cfdafacb6b080b110d8f8
kietta <truonganhkietdev@gmail.com>
2025-04-17 21:16:07 +0700

    add IRepos to IUnitOfWork

--- GalaxyBooking-BE/DAL/Repository/Interface/IUnitOfWork.cs
+++ GalaxyBooking-BE/DAL/Repository/Interface/IUnitOfWork.cs
@@ -2,6 +2,20 @@
 {
     public interface IUnitOfWork
     {
+        public IUserRepository UserRepository { get; }
+
+        public IRoomRepository RoomRepository { get; }
+
+        public ISeatRepository SeatRepository { get; }
+
+        public ITicketRepository TicketRepository { get; }
+
+        public IFilmRepository FilmRepository { get; }
+
+        public IGenreRepository GenreRepository { get; }
+
+        public IProjectionRepository ProjectionRepository { get; }
+
         Task SaveAsync();
     }
 }
