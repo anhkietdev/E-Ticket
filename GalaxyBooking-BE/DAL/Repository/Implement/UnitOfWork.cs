bc4ff5a0f0d49610e2aff3407c045b65c55ae55d
kietta <truonganhkietdev@gmail.com>
2025-04-18 10:35:48 +0700

    add blank Services

--- GalaxyBooking-BE/DAL/Repository/Implement/UnitOfWork.cs
+++ GalaxyBooking-BE/DAL/Repository/Implement/UnitOfWork.cs
@@ -6,9 +6,31 @@ namespace DAL.Repository.Implement
     internal class UnitOfWork : IUnitOfWork
     {
         private readonly AppDbContext _context;
+
+        public IUserRepository UserRepository { get; private set; }
+
+        public IRoomRepository RoomRepository { get; private set; }
+
+        public ISeatRepository SeatRepository { get; private set; }
+
+        public ITicketRepository TicketRepository { get; private set; }
+
+        public IFilmRepository FilmRepository { get; private set; }
+
+        public IGenreRepository GenreRepository { get; private set; }
+
+        public IProjectionRepository ProjectionRepository { get; private set; }
+
         public UnitOfWork(AppDbContext context)
         {
             _context = context;
+            UserRepository = new UserRepository(_context);
+            RoomRepository = new RoomRepository(_context);
+            SeatRepository = new SeatRepository(_context);
+            TicketRepository = new TicketRepository(_context);
+            FilmRepository = new FilmRepository(_context);
+            GenreRepository = new GenreRepository(_context);
+            ProjectionRepository = new ProjectionRepository(_context);
         }
         public async Task SaveAsync()
         {
