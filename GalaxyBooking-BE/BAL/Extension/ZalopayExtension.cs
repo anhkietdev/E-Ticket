7edc9f0139f6415f5798c253c031236121ba2e74
Khanh Nguyen Dev <quckhnh2921@gmail.com>
2025-04-20 09:49:13 +0700

    add cors, add zalopay

---
+++ GalaxyBooking-BE/BAL/Extension/ZalopayExtension.cs
@@ -0,0 +1,56 @@
+﻿using BAL.Helpers;
+using BAL.Services.ZaloPay.Request;
+using BAL.Services.ZaloPay.Response;
+using Newtonsoft.Json;
+
+namespace BAL.Extension
+{
+    public static class ZalopayExtension
+    {
+        public static void MakeSignature(this CreateZalopayRequest request, string key)
+        {
+            var data = $"{request.AppId}|{request.AppTransId}|{request.AppUser}|{request.Amount}|{request.AppTime}||";
+            request.Mac = HashHelper.HmacSha256(key, data);
+        }
+
+        public static Dictionary<string, string> GetContent(this CreateZalopayRequest request)
+        {
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            keyValuePairs.Add("appid", request.AppId.ToString());
+            keyValuePairs.Add("appuser", request.AppUser.ToString());
+            keyValuePairs.Add("apptime", request.AppTime.ToString());
+            keyValuePairs.Add("amount", request.Amount.ToString());
+            keyValuePairs.Add("apptransid", request.AppTransId);
+            keyValuePairs.Add("description", request.Description);
+            keyValuePairs.Add("bankcode", "zalopayapp");
+            keyValuePairs.Add("mac", request.Mac);
+
+            return keyValuePairs;
+        }
+        public static (bool, string) GetLink(this CreateZalopayRequest request, string paymentUrl)
+        {
+            using var client = new HttpClient();
+            var content = new FormUrlEncodedContent(GetContent(request));
+            var response = client.PostAsync(paymentUrl, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+                var responseData = JsonConvert.DeserializeObject<CreateZalopayResponse>(responseContent);
+
+                if (responseData.ReturnCode == Constants.Constant.ZaloPayConfig.ZaloPaymentSuccessStatus)
+                {
+                    return (true, responseData.OrderUrl);
+                }
+                else
+                {
+                    return (false, responseData.ReturnMessage);
+                }
+            }
+            else
+            {
+                return (false, response.ReasonPhrase ?? string.Empty);
+            }
+        }
+    }
+}
---
+++ GalaxyBooking-BE/BAL/Services/ZaloPay/Request/CreateZalopayRequest.cs
@@ -0,0 +1,19 @@
+﻿using BAL.Helpers;
+using BAL.Services.ZaloPay.Response;
+using Newtonsoft.Json;
+namespace BAL.Services.ZaloPay.Request
+{
+    public class CreateZalopayRequest
+    {
+        public int? AppId { get; set; }
+        public string AppUser { get; set; }
+        public long AppTime { get; set; }
+        public long Amount { get; set; }
+        public string AppTransId { get; set; } = string.Empty;
+        public string ReturnUrl { get; set; }
+        public string EmbedData { get; set; } = string.Empty;
+        public string Mac { get; set; } = string.Empty;
+        public string BankCode { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
