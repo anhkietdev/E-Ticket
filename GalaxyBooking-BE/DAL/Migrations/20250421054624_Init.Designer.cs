7544f9194263fc1e4370f94d4f076b5ffd31c7ae
kietta <truonganhkietdev@gmail.com>
2025-04-21 12:50:38 +0700

    add Email Service and IdentityUser model

--- GalaxyBooking-BE/DAL/Migrations/20250421054624_Init.Designer.cs
+++ GalaxyBooking-BE/DAL/Migrations/20250421054624_Init.Designer.cs
@@ -4,6 +4,7 @@ using DAL.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250421054624_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -163,7 +166,7 @@ namespace DAL.Migrations
 
                     b.HasKey("UserId");
 
-                    b.ToTable("IdentityUsers");
+                    b.ToTable("IdentityUser");
                 });
 
             modelBuilder.Entity("DAL.Models.Projection", b =>
