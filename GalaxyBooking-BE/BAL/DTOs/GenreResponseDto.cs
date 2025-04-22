d8a9a512f22bf9450d906f955c75239a927fcb88
Trí Sỹ Trương <thefoxno4@gmail.com>
2025-04-23 02:00:09 +0700

    add genre api

---
+++ GalaxyBooking-BE/BAL/DTOs/GenreResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.DTOs
+{
+    public class GenreResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
