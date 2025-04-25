43c4aa19a3c948adee30a351e2d2bad81e612d3c
SuB <128241521+300baicodethieunhicungSyDauBu@users.noreply.github.com>
2025-04-25 20:36:08 +0700

    Revert "update create/update projection api"

--- GalaxyBooking-BE/BAL/DTOs/ProjectionRequestDto.cs
+++ GalaxyBooking-BE/BAL/DTOs/ProjectionRequestDto.cs
@@ -1,33 +1,22 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BAL.DTOs
 {
     public class ProjectionRequestDto
     {
-        [Required]
-        [RegularExpression(@"^\d{4}-\d{2}-\d{2}$", ErrorMessage = "StartDate must be in 'yyyy-MM-dd' format")]
-        public string StartDate { get; set; } = DateTime.Now.ToString("yyyy-MM-dd"); // e.g., "2025-04-25"
+        public DateTime StartTime { get; set; }
 
-        [Required]
-        [RegularExpression(@"^\d{2}:\d{2}:\d{2}$", ErrorMessage = "StartTime must be in 'HH:mm:ss' format")]
-        public string StartTime { get; set; } = DateTime.Now.ToString("HH:mm:ss"); // e.g., "09:15:24"
+        public DateTime EndTime { get; set; }
 
-        [Required]
-        [RegularExpression(@"^\d{4}-\d{2}-\d{2}$", ErrorMessage = "EndDate must be in 'yyyy-MM-dd' format")]
-        public string EndDate { get; set; } = DateTime.Now.ToString("yyyy-MM-dd"); // e.g., "2025-04-25"
-
-        [Required]
-        [RegularExpression(@"^\d{2}:\d{2}:\d{2}$", ErrorMessage = "EndTime must be in 'HH:mm:ss' format")]
-        public string EndTime { get; set; } = DateTime.Now.AddHours(2).ToString("HH:mm:ss"); // e.g., "11:15:24"
-
-        [Required]
         public decimal Price { get; set; }
 
-        [Required]
         public Guid FilmId { get; set; }
 
-        [Required]
         public Guid RoomId { get; set; }
     }
 }
