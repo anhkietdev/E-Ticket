4d8755aef57d43b08c79ba336216febc4b8bafa1
Khanh Nguyen Dev <quckhnh2921@gmail.com>
2025-04-28 01:20:42 +0700

    config program.cs

--- GalaxyBooking-BE/Presentation/Program.cs
+++ GalaxyBooking-BE/Presentation/Program.cs
@@ -13,11 +13,9 @@ builder.Services.ResolveDAL(connectionString)
     .AddJwtSwagger();
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
