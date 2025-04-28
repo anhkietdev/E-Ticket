7a87e9525a3abc0a513b8a90f224148305e574b8
Trí Sỹ Trương <thefoxno4@gmail.com>
2025-04-28 14:33:04 +0700

    update seat api

--- GalaxyBooking-BE/BAL/DTOs/SeatResponseDto.cs
+++ GalaxyBooking-BE/BAL/DTOs/SeatResponseDto.cs
@@ -8,6 +8,7 @@ namespace BAL.DTOs
         public Guid Id { get; set; }
         public required string SeatNumber { get; set; }
         public required string Row { get; set; }
+        public bool IsEnable { get; set; } = true;
         public Guid RoomId { get; set; }
         public RoomDto Room { get; set; }
         public ICollection<TicketDto> Tickets { get; set; }
