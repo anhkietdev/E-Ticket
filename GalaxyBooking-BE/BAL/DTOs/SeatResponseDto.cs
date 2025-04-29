b455c4aeb110c2632b45273aa28be98181a7d958
Khanh Nguyen Dev <quckhnh2921@gmail.com>
2025-04-29 09:06:25 +0700

    add is vip

--- GalaxyBooking-BE/BAL/DTOs/SeatResponseDto.cs
+++ GalaxyBooking-BE/BAL/DTOs/SeatResponseDto.cs
@@ -9,6 +9,7 @@ namespace BAL.DTOs
         public required string SeatNumber { get; set; }
         public required string Row { get; set; }
         public bool IsEnable { get; set; } = true;
+        public bool IsVip { get; set; } = true;
         public Guid RoomId { get; set; }
         public RoomDto Room { get; set; }
         public ICollection<TicketDto> Tickets { get; set; }
